5030204002e71f15de5044d2ae874fd213cb063b
Abenx <rushairer@163.com>
2018-05-14 18:07:43 +0800

    [Modify]coverage config

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,13 @@ module.exports = {
       port: 8545,
       network_id: '*', // eslint-disable-line camelcase
     },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
     ganache: {
       host: 'localhost',
       port: 8545,
