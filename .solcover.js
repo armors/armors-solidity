99eedd9f75f43e83867a0b0304ec920c8e342580
Abenx <rushairer@163.com>
2018-05-14 18:38:15 +0800

    [Modify]solcover

--- .solcover.js
+++ .solcover.js
@@ -3,6 +3,7 @@ module.exports = {
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
     skipFiles: [
+        'DemoToken.sol',
         'mocks'
     ]
 }
