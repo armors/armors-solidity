ef5c6c3f180673c3128223d01337e992f6c2f78d
Abenx <rushairer@163.com>
2018-05-22 12:16:29 +0800

    [Add]SafeERC20.sol

---
+++ contracts/SafeERC20.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.23;
+
+import "./ERC20Basic.sol";
+import "./ERC20.sol";
+
+
+library SafeERC20 {
+  
+  function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+    require(token.transfer(to, value));
+  }
+
+  function safeTransferFrom(
+    ERC20 token,
+    address from,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    require(token.transferFrom(from, to, value));
+  }
+
+  function safeApprove(ERC20 token, address spender, uint256 value) internal {
+    require(token.approve(spender, value));
+  }
+}
